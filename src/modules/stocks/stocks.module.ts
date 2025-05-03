d364256fdc8b6ac76a184788c95b3fe55a56468c
Eder <eder.j12@gmail.com>
2025-05-03 15:47:51 -0600

    feat: add VendorModule to stocks and transactions modules for improved vendor integration

--- src/modules/stocks/stocks.module.ts
+++ src/modules/stocks/stocks.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Stock } from './entities/stock.entity';
 import { StockRepository } from './repositories/stock.repository';
+import { VendorModule } from '@modules/shared/vendor/vendor.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Stock])],
+  imports: [TypeOrmModule.forFeature([Stock]), VendorModule],
   controllers: [],
   providers: [StockRepository],
   exports: [StockRepository],
--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
 import { TransactionRepository } from './repositories/transaction.repository';
+import { VendorModule } from '@modules/shared/vendor/vendor.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction])],
+  imports: [TypeOrmModule.forFeature([Transaction]), VendorModule],
   controllers: [],
   providers: [TransactionRepository],
   exports: [TransactionRepository],
