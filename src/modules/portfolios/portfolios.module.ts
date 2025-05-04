c80ba3dc0bdefeb8c1cc7da5f109cbe8c1b04ce5
Eder <eder.j12@gmail.com>
2025-05-04 16:20:19 -0600

    feat: add UsersModule to portfolios module imports for user-related functionality

--- src/modules/portfolios/portfolios.module.ts
+++ src/modules/portfolios/portfolios.module.ts
@@ -4,9 +4,10 @@ import { Portfolio } from './entities/portfolio.entity';
 import { PortfolioHolding } from './entities/portfolio-holding.entity';
 import { PortfolioRepository } from './repositories/portfolio.repository';
 import { PortfolioHoldingRepository } from './repositories/portfolio-holding.repository';
+import { UsersModule } from '@/modules/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Portfolio, PortfolioHolding])],
+  imports: [TypeOrmModule.forFeature([Portfolio, PortfolioHolding]), UsersModule],
   providers: [PortfolioRepository, PortfolioHoldingRepository],
   exports: [PortfolioRepository, PortfolioHoldingRepository],
 })
